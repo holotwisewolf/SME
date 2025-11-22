a366313a1e33d05ff10564236016eb4af3d9a0d2
YJ <choongyj-wm24@student.tarc.edu.my>
2025-11-22 09:08:31 -0500

    did better UI

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchButton from './SearchButton';
+import ClearButton from './ClearButton';
+
 
 const SearchBar: React.FC = () => {
+  // State to handle the input value
+  const [searchText, setSearchText] = useState('');
+
+  // Function to clear the search text
+  const handleClear = () => {
+    setSearchText('');
+  };
+
   return (
     <div className="flex-1 max-w-[600px] min-w-[200px]">
       <div className="relative flex items-center w-full h-12">
@@ -9,9 +19,29 @@ const SearchBar: React.FC = () => {
         <div className="absolute left-3 top-1/2 -translate-y-1/2 z-10 flex items-center justify-center">
         <SearchButton />      
         </div>
+
+        {/* --- CLEAR BUTTON (Right, only shows when text exists) --- */}
+        {searchText && (
+          <button 
+            onClick={handleClear}
+            // Position: right-32 (approx 128px) puts it to the left of the dropdown
+            className="absolute right-32 top-1/2 -translate-y-1/2 z-20 p-1 
+            rounded-full 
+            hover:bg-white/20
+            hover:scale-110 
+            transition-all 
+            focus:outline-none"
+            title="Clear Search"
+          >
+            <ClearButton />
+          </button>
+        )}
+
         <input
           type="text"
           placeholder="Search in here"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
           className="w-full h-full bg-[#555555] text-white placeholder-gray-300 rounded-full pl-12 pr-36 border border-white focus:outline-none focus:ring-1 focus:ring-white transition"
         />
 
--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -16,8 +16,7 @@ const SearchButton: React.FC = () => {
       >
         <path 
           d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" 
-          // CHANGED: From #000000 to #FFD1D1 (Pink) or use #FFFFFF (White)
-          stroke="#000000" 
+          stroke="white" 
           strokeWidth="2" 
           strokeLinecap="round" 
           strokeLinejoin="round"
