685f5c488351a396f1400c006ed7c86e78e3a10a
YJ <choongyj-wm24@studen.tarc.edu.my>
2025-11-25 14:41:44 -0500

    Login overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useLocation, Routes, Route, Navigate } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
+import { LoginProvider } from "./components/login/LoginProvider";
+import LoginDrawer from "./components/login/LoginDrawer";
+
 import Layout from "./components/shared/Layout";
 import Header from "./components/header/Header";
 import PageWrapper from "./components/shared/PageWrapper";
 
 // Pages
-import LoginPage from "./pages/auth_pages/LoginPage";
 import LibraryPlaylists from "./pages/library_pages/LibraryPlaylists";
 import LibraryTracks from "./pages/library_pages/LibraryTracks";
 import LibraryAlbums from "./pages/library_pages/LibraryAlbums";
@@ -20,7 +22,7 @@ function App() {
   const location = useLocation();
 
   return (
-    <>
+    <LoginProvider>
       <Layout>
         <div className="flex flex-col h-full">
 
@@ -33,18 +35,13 @@ function App() {
           <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
 
-              {/* LOGIN (fullscreen drawer, no wrapper) */}
-              <Route path="/login" element={<LoginPage />} />
-
-              {/* HOME - Redirect to Library Playlists */}
+              {/* HOME → redirect */}
               <Route path="/" element={<Navigate to="/library/playlists" replace />} />
 
-              {/* --------------------------- */}
-              {/*         LIBRARY             */}
-              {/* --------------------------- */}
+              {/* LIBRARY --------------------- */}
               <Route path="/library">
-                {/* Default: redirect to playlists */}
                 <Route index element={<Navigate to="playlists" replace />} />
+
                 <Route
                   path="playlists"
                   element={
@@ -71,12 +68,10 @@ function App() {
                 />
               </Route>
 
-              {/* --------------------------- */}
-              {/*         FAVOURITES          */}
-              {/* --------------------------- */}
+              {/* FAVOURITES ------------------ */}
               <Route path="/favourites">
-                {/* Default: redirect to playlists */}
                 <Route index element={<Navigate to="playlists" replace />} />
+
                 <Route
                   path="playlists"
                   element={
@@ -125,10 +120,11 @@ function App() {
 
             </Routes>
           </AnimatePresence>
-
         </div>
       </Layout>
-    </>
+
+      <LoginDrawer />
+    </LoginProvider>
   );
 }
 
