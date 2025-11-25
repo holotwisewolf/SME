9256a24e738d396b97179d45dd5885db8b886aca
YJ <choongyj-wm24@studen.tarc.edu.my>
2025-11-25 09:42:08 -0500

    added placeholder pages

---
+++ src/pages/favorites/FavoritesTracks.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoritesTracks: React.FC = () => {
+    return (
+        <div className="text-white text-2xl p-8">
+            <h1>Favorites Tracks</h1>
+        </div>
+    );
+};
+
+export default FavoritesTracks;
---
+++ src/pages/library/LibraryAlbums.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LibraryAlbums: React.FC = () => {
+    return (
+        <div className="text-white text-2xl p-8">
+            <h1>Library Albums</h1>
+        </div>
+    );
+};
+
+export default LibraryAlbums;
