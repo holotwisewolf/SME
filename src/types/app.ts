94b14b960e5849e27ba59db31c077cad068cbb23
Lee Shen Fung <leeshenfung@gmail.com>
2025-11-26 17:26:24 +0800

    Added Comment,CommentInsert,CommentUpdate in ../../types/app.ts

--- src/types/app.ts
+++ src/types/app.ts
@@ -7,3 +7,6 @@ export type Rating = Database['public']['Tables']['ratings']['Row'];
 export type RatingInsert = Database['public']['Tables']['ratings']['Insert'];
 export type RatingUpdate = Database['public']['Tables']['ratings']['Update'];
 export type ItemStats = Database['public']['Tables']['item_stats']['Row'];
+export type Comment = Database['public']['Tables']['comments']['Row'];
+export type CommentInsert = Database['public']['Tables']['comments']['Insert'];
+export type CommentUpdate = Database['public']['Tables']['comments']['Update'];
